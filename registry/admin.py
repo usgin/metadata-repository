d983edee38ca3d6183e3652cbd42b5c7c5f68393
Ryan Clark <ryan.clark.j@gmail.com>
2012-07-31 11:58:55 -0500

    Some simple admin adjustments

--- registry/admin.py
+++ registry/admin.py
@@ -2,12 +2,15 @@ from models import Resource, ResourceCollection, Contact
 from django.contrib import admin
 
 class ResourceAdmin(admin.ModelAdmin):
-    list_display = ('title', 'metadata_link', 'published')
+    list_display = ('title', 'metadata_id', 'metadata_link', 'published')
     filter_horizontal = ['editors', 'collections']
     list_filter = ('published', 'collections')
-
+    search_fields = ['title', 'metadata_id']
+    
 class ResourceCollectionAdmin(admin.ModelAdmin):
     filter_horizontal = [ 'editors', 'parents' ]
+    list_display = ('title', 'collection_id')
+    search_fields = ['title', 'collection_id']
     
 admin.site.register(Resource, ResourceAdmin)
 admin.site.register(ResourceCollection, ResourceCollectionAdmin)
