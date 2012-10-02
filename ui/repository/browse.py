a8218a1c20ea48a3392ef9e6d898a73eb9642d9c
Ryan Clark <ryan.clark.j@gmail.com>
2012-10-02 14:02:09 -0500

    Adjust parent filter on collections. Now top-level collections should specific 'top' as their parent instead of being null. This helps get rid of the problem of collections ending up being top-level when removed from their old parent

--- ui/repository/browse.py
+++ ui/repository/browse.py
@@ -6,7 +6,7 @@ import json
 
 def browse(req):
     # Find all the collections that do not have parents
-    top = ResourceCollection.objects.filter(parents__isnull=True)
+    top = ResourceCollection.objects.filter(parents="Top")
     
     # Find the closure (all children) for each top-level collection
     result = [ col.jsonClosure(req.user) for col in top ]
