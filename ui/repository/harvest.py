86361ee6b4eb81f6cf11c98d1957305c73bc09f8
Genhan Chen <genhan.chen@azgs.az.gov>
2013-02-01 13:53:56 -0700

    Delete conflict line in harvest.py

--- ui/repository/harvest.py
+++ ui/repository/harvest.py
@@ -13,17 +13,5 @@ def harvest(req):
             self.key = key
             self.label = label
     collections = [ { 'id': col.collection_id, 'value': col.title } for col in [ rs for rs in ResourceCollection.objects.all() if rs.can_edit(req.user) ] ]        
-<<<<<<< HEAD
     context = { 'formats': [ Format('iso.xml', 'USGIN ISO 19139'), Format('atom.xml', 'Atom Feed'), Format('fgdc.xml', 'FGDC'), Format('csv', 'CSV') ], 'collections': json.dumps(collections) }
-=======
-    context = { 
-        'formats': [ 
-          Format('iso.xml', 'USGIN ISO 19139'), 
-          Format('atom.xml', 'Atom Feed'), 
-          Format('fgdc.xml', 'FGDC XML'), 
-          Format('csv', 'CSV Metadata Template') 
-        ], 
-        'collections': json.dumps(collections) 
-      }
->>>>>>> 20fe91746b50d4a20fbe404f56870a24d2d9ab4a
     return render_to_response('repository/harvest.jade', context, context_instance=RequestContext(req))
