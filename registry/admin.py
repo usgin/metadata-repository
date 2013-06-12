2da9264598196a031c9fa82fa668a717c1467501
Ryan Clark <ryan.clark.j@gmail.com>
2013-06-12 11:06:53 -0700

    Change admin action so that marking things as published affects the couchdb and not just django

--- registry/admin.py
+++ registry/admin.py
@@ -1,8 +1,33 @@
 from models import Resource, ResourceCollection, Contact
 from django.contrib import admin
+from metadatadb.proxy import oneResource, proxyRequest
+import json
 
 def make_published(modeladmin, request, queryset):
-    queryset.update(published=True)
+    # Have to update CouchDB too...
+    class DummyRequest():
+        def __init__(self, method, user, body="{}"):
+            self.method = method
+            self.user = user
+            self.body = body
+
+    for record in queryset:
+        # Get the full Record
+        kwargs = {
+            'path': '/metadata/record/' + record.metadata_id + '/',
+            'method': 'GET'
+        }
+        original = proxyRequest(**kwargs)
+
+        # Update it
+        final = json.loads(original.content)
+        final["Published"] = True
+
+        # Send the update
+        update = DummyRequest("PUT", request.user, json.dumps(final))
+        result = oneResource(update, "record", record.metadata_id)
+        print result
+
 make_published.short_description = "Mark selected resources as published"
 
 class ResourceAdmin(admin.ModelAdmin):
